f8e455e00680d18d640a2a06c020e570b265d378
JJOELJOAQUIN <lopezjoeljoaquin3@gmail.com>
2025-04-11 12:00:00 -0300

    preview.jpg para ig

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import type { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,35 @@ const geistMono = Geist_Mono({
 });
 
 
+export const metadata: Metadata = {
+  title: "Joel Joaquín López | Full-Stack Developer",
+  description:
+    "Desarrollos de software a medida, combinando tecnología e innovación para alcanzar tus objetivos.",
+  openGraph: {
+    title: "Joel Joaquín López | Full-Stack Developer",
+    description:
+      "Desarrollos de software a medida, combinando tecnología e innovación para alcanzar tus objetivos.",
+    url: "https://portfolio-front-eight.vercel.app",
+    siteName: "Joel Joaquín López Portfolio",
+    images: [
+      {
+        url: "https://portfolio-front-eight.vercel.app/preview.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "Imagen de presentación del portfolio de Joel Joaquín López",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Joel Joaquín López | Full-Stack Developer",
+    description:
+      "Desarrollos de software a medida, combinando tecnología e innovación para alcanzar tus objetivos.",
+    images: ["https://portfolio-front-eight.vercel.app/preview.jpg"],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -21,13 +51,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
